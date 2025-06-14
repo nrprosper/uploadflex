27114db22ff7f96b5ffd2e16a81a9b298b8e473d
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2025-06-14 19:52:54 +0200

    ft: add cludinary and minio config

--- src/main/java/org/devkiki/uploadflex/config/CloudinaryConfig.java
+++ src/main/java/org/devkiki/uploadflex/config/CloudinaryConfig.java
@@ -1,4 +1,31 @@
 package org.devkiki.uploadflex.config;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class CloudinaryConfig {
+
+    @Value("${cloudinary.api_key}")
+    private String API_KEY;
+    @Value("${cloudinary.api_secret}")
+    private String API_SECRET;
+    @Value("${cloudinary.cloud_name}")
+    private String CLOUD_NAME;
+
+
+    @Bean
+    public Cloudinary cloudinary() {
+        return new Cloudinary(
+                ObjectUtils.asMap(
+                        "cloud_name", CLOUD_NAME,
+                        "api_key", API_KEY,
+                        "api_secret", API_SECRET
+                )
+        );
+    }
+
 }
--- src/main/java/org/devkiki/uploadflex/config/MinioConfig.java
+++ src/main/java/org/devkiki/uploadflex/config/MinioConfig.java
@@ -1,4 +1,26 @@
 package org.devkiki.uploadflex.config;
 
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class MinioConfig {
+
+    @Value("${minio.url}")
+    private String MINIO_ENDPOINT;
+    @Value("${minio.access_key}")
+    private String MINIO_ACCESS_KEY;
+    @Value("${minio.secret_key}")
+    private String MINIO_SECRET_KEY;
+
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                .endpoint(MINIO_ENDPOINT)
+                .credentials(MINIO_ACCESS_KEY, MINIO_SECRET_KEY)
+                .build();
+    }
+
 }
