f66a90d80e6fc36620810d9ab73e9dcec87237a8
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2025-06-14 19:17:50 +0200

    chore: initialise project and dockerized

---
+++ src/main/java/org/devkiki/uploadflex/UploadFlex.java
@@ -0,0 +1,13 @@
+package org.devkiki.uploadflex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadFlex {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UploadFlex.class, args);
+    }
+
+}
---
+++ src/test/java/org/devkiki/uploadflex/UploadflexApplicationTests.java
@@ -0,0 +1,13 @@
+package org.devkiki.uploadflex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadflexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
